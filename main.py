88a6a2a9946c62ab1e4b2e2339cedccf71965ed8
WilliamTung <williamtung.work@gmail.com>
2023-03-07 23:54:30 +0800

    Add some new endpoint Add /register GET, /habit for POST and GET

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def test():
 	return "Hello World!"
 
+@app.route("/register", methods=['POST'])
+def RegisterUser():
+	raise NotImplementedError
+
+@app.route("/habit", methods = ['GET'])
+def Gethabit():
+	raise NotImplementedError
+
+@app.route("/habit", methods = ['POST'])
+def AddNewHabit():
+	raise NotImplementedError
+
 if __name__ == "__main__":
 	app.run(debug=True)
